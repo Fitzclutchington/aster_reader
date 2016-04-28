16b21874a7dfa737359c3a983beabd023f504f04
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2016-04-28 14:31:44 -0400

    histogram matching started

--- aster_convert.py
+++ aster_convert.py
@@ -102,6 +102,55 @@ def getRadius(lat):
   return np.sqrt((np.square(a*a*np.cos(lat)) + np.square(b*b*np.sin(lat)))/ \
                  (np.square(a*np.cos(lat)) + np.square(b*np.sin(lat))) )
 
+def hist_match(source, template):
+    """
+    Adjust the pixel values of a grayscale image such that its histogram
+    matches that of a target image
+
+    Arguments:
+    -----------
+        source: np.ndarray
+            Image to transform; the histogram is computed over the flattened
+            array
+        template: np.ndarray
+            Template image; can have different dimensions to source
+    Returns:
+    -----------
+        matched: np.ndarray
+            The transformed output image
+    """
+
+    oldshape = source.shape
+    source = source.ravel()
+    template = template.ravel()
+
+    # get the set of unique pixel values and their corresponding indices and
+    # counts
+    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                            return_counts=True)
+    t_values, t_counts = np.unique(template, return_counts=True)
+
+    # take the cumsum of the counts and normalize by the number of pixels to
+    # get the empirical cumulative distribution functions for the source and
+    # template images (maps pixel value --> quantile)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+
+    # interpolate linearly to find the pixel values in the template image
+    # that correspond most closely to the quantiles in the source image
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+    return interp_t_values[bin_idx].reshape(oldshape)
+
+def ecdf(x):
+    """convenience function for computing the empirical CDF"""
+    vals, counts = np.unique(x, return_counts=True)
+    ecdf = np.cumsum(counts).astype(np.float64)
+    ecdf /= ecdf[-1]
+    return vals, ecdf
+
 if __name__=="__main__":
    
   filename = sys.argv[1] 
@@ -169,48 +218,12 @@ if __name__=="__main__":
   reflectance_b3 = RadToRefl ( DnToRad ( db3N, 3, getGain(hdf,'3N')), 3, earth_sun_dist, sza)
   rfb3 = reflectance_b3 / np.cos(sza)
   
-  #np.dstack((rfb1,rfb2,rfb3))
-  
-  """
-  plt.figure()
-  plt.title("Modis band 1")
-  plt.imshow(pbands[0])
-  plt.colorbar()
-  
-  plt.figure()
-  plt.title("Aster band 2")
-  plt.imshow ( reflectance_b2 ) #vmin=-0.2, vmax=0.8, interpolation='nearest')
-  plt.colorbar()
-  
-  plt.figure()
-  plt.title("Modis band 2")
-  plt.imshow(pbands[1])
-  plt.colorbar()
+  rfb1_match = hist_match(reflectance_b1,pbands[3])
+  rfb2_match = hist_match(reflectance_b2,pbands[0])
+  rfb3_match = hist_match(reflectance_b3,pbands[1])  
 
-  plt.figure()
-  plt.title("Modis band 4")
-  plt.imshow(pbands[3])
-  plt.colorbar()      
-
-  plt.figure()
-  plt.title("Aster band 1")
-  plt.imshow ( reflectance_b1 ) #vmin=-0.2, vmax=0.8, interpolation='nearest')
-  plt.colorbar()
-
-  plt.figure()
-  plt.title("Aster band 2")
-  plt.imshow ( reflectance_b2 ) #vmin=-0.2, vmax=0.8, interpolation='nearest')
-  plt.colorbar()
-
-  plt.figure()
-  plt.title("Aster band 3N")
-  plt.imshow ( reflectance_b3 ) #vmin=-0.2, vmax=0.8, interpolation='nearest')
-  plt.colorbar()
-  plt.show()
-  """
-  
-  fig, axarr = plt.subplots(2,3, figsize=(15,10))
-  img1 = axarr[0,0].imshow(pbands[3])
+  fig, axarr = plt.subplots(3,3, figsize=(30,20))
+  img00 = axarr[0,0].imshow(pbands[3], vmin=0,vmax=1)
   axarr[0,0].set_title('Modis band 4')
   div1 = make_axes_locatable(axarr[0,0])
   cax1 = div1.append_axes("right", size="15%", pad=0.05)
@@ -218,17 +231,29 @@ if __name__=="__main__":
   axarr[0,0].xaxis.set_visible(False)
   axarr[0,0].yaxis.set_visible(False)
 
+  
 
-
-  img2 = axarr[1,0].imshow(reflectance_b1)
+  img10 = axarr[1,0].imshow(rfb1_match, vmin=0,vmax=1)
   axarr[1,0].set_title('Aster band 1')
   div2 = make_axes_locatable(axarr[1,0])
   cax2 = div2.append_axes("right", size="15%", pad=0.05)
   cbar2 = plt.colorbar(img2, cax=cax2)
   axarr[1,0].xaxis.set_visible(False)
   axarr[1,0].yaxis.set_visible(False)
+  
+  x1, y1 = ecdf(reflectance_b1.ravel())
+  x2, y2 = ecdf(pbands[3].ravel())
+  x3, y3 = ecdf(rfb1_match.ravel())
 
-  img3 = axarr[0,1].imshow(pbands[0])
+  axarr[2,0].plot(x1,y1,'-r',label="aster")
+  axarr[2,0].plot(x2,y2,'-k',label="modis")
+  axarr[2,0].plot(x3,y3,'-b',label="match")
+  axarr[2,0].set_title('Aster 1 Modis 4 Matched Histogram')
+  axarr[2,0].set_xlabel('Pixel value')
+  axarr[2,0].set_ylabel('Cumulative %')
+  axarr[2,0].legend(loc=5)
+
+  img3 = axarr[0,1].imshow(pbands[0], vmin=0,vmax=1)
   axarr[0,1].set_title('Modis band 1')
   div3 = make_axes_locatable(axarr[0,1])
   cax3 = div3.append_axes("right", size="15%", pad=0.05)
@@ -236,15 +261,27 @@ if __name__=="__main__":
   axarr[0,1].xaxis.set_visible(False)
   axarr[0,1].yaxis.set_visible(False)
 
-  img4 = axarr[1,1].imshow(reflectance_b2)
+  img4 = axarr[1,1].imshow(rfb2_match, vmin=0,vmax=1)
   axarr[1,1].set_title('Aster band 2')
   div4 = make_axes_locatable(axarr[1,1])
   cax4 = div4.append_axes("right", size="15%", pad=0.05)
   cbar4 = plt.colorbar(img4, cax=cax4)
   axarr[1,1].xaxis.set_visible(False)
   axarr[1,1].yaxis.set_visible(False)
+  
+  x1, y1 = ecdf(reflectance_b2.ravel())
+  x2, y2 = ecdf(pbands[0].ravel())
+  x3, y3 = ecdf(rfb2_match.ravel())
 
-  img5 = axarr[0,2].imshow(pbands[1])
+  axarr[2,1].plot(x1,y1,'-r',label="aster")
+  axarr[2,1].plot(x2,y2,'-k',label="modis")
+  axarr[2,1].plot(x3,y3,'-b',label="match")
+  axarr[2,1].set_title('Aster 2 Modis 1 Matched Histogram')
+  axarr[2,1].set_xlabel('Pixel value')
+  axarr[2,1].set_ylabel('Cumulative %')
+  axarr[2,1].legend(loc=5)
+
+  img5 = axarr[0,2].imshow(pbands[1], vmin=0,vmax=1)
   axarr[0,2].set_title('Modis band 2')
   div5 = make_axes_locatable(axarr[0,2])
   cax5 = div5.append_axes("right", size="15%", pad=0.05)
@@ -253,13 +290,25 @@ if __name__=="__main__":
   axarr[0,2].yaxis.set_visible(False)
 
 
-  img6 = axarr[1,2].imshow(reflectance_b3)
+  img6 = axarr[1,2].imshow(rfb3_match, vmin=0,vmax=1)
   axarr[1,2].set_title('Aster band 3N')
   div6 = make_axes_locatable(axarr[1,2])
   cax6 = div6.append_axes("right", size="15%", pad=0.05)
   cbar6 = plt.colorbar(img6, cax=cax6)
   axarr[1,2].xaxis.set_visible(False)
   axarr[1,2].yaxis.set_visible(False)
+  
+  x1, y1 = ecdf(reflectance_b3.ravel())
+  x2, y2 = ecdf(pbands[1].ravel())
+  x3, y3 = ecdf(rfb3_match.ravel())
 
-  plt.show()
+  axarr[2,2].plot(x1,y1,'-r',label="aster")
+  axarr[2,2].plot(x2,y2,'-k',label="modis")
+  axarr[2,2].plot(x3,y3,'-b',label="match")
+  axarr[2,2].set_title('Aster 3 Modis 2 Matched Histogram')
+  axarr[2,2].set_xlabel('Pixel value')
+  axarr[2,2].set_ylabel('Cumulative %')
+  axarr[2,2].legend(loc=5)
+
   fig.savefig('modis_aster_projection.png')
+  plt.close()
