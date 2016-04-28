d1cfa1e8468d631bc63f5d2f1c990d02c97039e0
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2016-04-28 18:50:18 -0400

    hist to histogram

--- calculations.py
+++ calculations.py
@@ -80,7 +80,7 @@ def hist_match2(Z,h,bins):
     g = np.nan*np.ones((1,l))
     cdf = np.cumsum(h)
 
-    hQ = np.hist(Z,bins=bins)
+    hQ = np.histogram(Z,bins=bins)
     cdfQ = cumsum(hQ)
 
     c=0; kQ=1
